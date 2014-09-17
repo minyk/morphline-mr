c2c33311a7faa2d4c13fdc08889eb9d7e58d332f
drakemin <drake.min@nexr.com>
2014-09-17 14:58:25 +0900

    Add IgnoreKeyTextFormat class

--- src/main/java/com/example/minyk/MorphlineMRDriver.java
+++ src/main/java/com/example/minyk/MorphlineMRDriver.java
@@ -1,6 +1,8 @@
 package com.example.minyk;
 
+import com.example.minyk.mapper.IgnoreKeyOutputFormat;
 import com.example.minyk.mapper.MorphlineMapper;
+import com.typesafe.config.ConfigException;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.NullWritable;
@@ -21,10 +23,11 @@ public class MorphlineMRDriver {
         job.setJarByClass(MorphlineMRDriver.class);
         job.setMapperClass(MorphlineMapper.class);
         job.setNumReduceTasks(0);
-        job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(NullWritable.class);
+        job.setOutputKeyClass(NullWritable.class);
+        job.setOutputValueClass(Text.class);
+        job.setOutputFormatClass(IgnoreKeyOutputFormat.class);
         FileInputFormat.addInputPath(job, new Path(args[0]));
-        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        IgnoreKeyOutputFormat.setOutputPath(job, new Path(args[1]));
         System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 }
---
+++ src/main/java/com/example/minyk/mapper/IgnoreKeyOutputFormat.java
@@ -0,0 +1,111 @@
+package com.example.minyk.mapper;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataOutputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.compress.CompressionCodec;
+import org.apache.hadoop.io.compress.GzipCodec;
+import org.apache.hadoop.mapreduce.RecordWriter;
+import org.apache.hadoop.mapreduce.TaskAttemptContext;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.apache.hadoop.util.ReflectionUtils;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
+/**
+ * Created by drake on 9/17/14.
+ */
+public class IgnoreKeyOutputFormat<K,V> extends TextOutputFormat<K,V> {
+    protected static class IgnoreKeyLineRecordWriter<K, V> extends RecordWriter<K, V> {
+        private static final String utf8 = "UTF-8";
+        private static final byte[] newline;
+        static {
+            try {
+                newline = "\n".getBytes(utf8);
+            } catch (UnsupportedEncodingException uee) {
+                throw new IllegalArgumentException("can't find " + utf8 + " encoding");
+            }
+        }
+
+        protected DataOutputStream out;
+        private final byte[] keyValueSeparator;
+
+        public IgnoreKeyLineRecordWriter(DataOutputStream out, String keyValueSeparator) {
+            this.out = out;
+            try {
+                this.keyValueSeparator = keyValueSeparator.getBytes(utf8);
+            } catch (UnsupportedEncodingException uee) {
+                throw new IllegalArgumentException("can't find " + utf8 + " encoding");
+            }
+        }
+
+        public IgnoreKeyLineRecordWriter(DataOutputStream out) {
+            this(out, "\t");
+        }
+
+        /**
+         * Write the object to the byte stream, handling Text as a special
+         * case.
+         * @param o the object to print
+         * @throws IOException if the write throws, we pass it on
+         */
+        private void writeObject(Object o) throws IOException {
+            if (o instanceof Text) {
+                Text to = (Text) o;
+                out.write(to.getBytes(), 0, to.getLength());
+            } else {
+                out.write(o.toString().getBytes(utf8));
+            }
+        }
+
+        public synchronized void write(K key, V value)
+                throws IOException {
+
+            boolean nullValue = value == null || value instanceof NullWritable;
+            if (nullValue) {
+                return;
+            }
+            if (!nullValue) {
+                writeObject(value);
+            }
+            out.write(newline);
+        }
+
+        public synchronized
+        void close(TaskAttemptContext context) throws IOException {
+            out.close();
+        }
+    }
+
+    public RecordWriter<K, V>
+    getRecordWriter(TaskAttemptContext job
+    ) throws IOException, InterruptedException {
+        Configuration conf = job.getConfiguration();
+        boolean isCompressed = getCompressOutput(job);
+        String keyValueSeparator= conf.get(SEPERATOR, "\t");
+        CompressionCodec codec = null;
+        String extension = "";
+        if (isCompressed) {
+            Class<? extends CompressionCodec> codecClass =
+                    getOutputCompressorClass(job, GzipCodec.class);
+            codec = (CompressionCodec) ReflectionUtils.newInstance(codecClass, conf);
+            extension = codec.getDefaultExtension();
+        }
+        Path file = getDefaultWorkFile(job, extension);
+        FileSystem fs = file.getFileSystem(conf);
+        if (!isCompressed) {
+            FSDataOutputStream fileOut = fs.create(file, false);
+            return new IgnoreKeyLineRecordWriter<K, V>(fileOut, keyValueSeparator);
+        } else {
+            FSDataOutputStream fileOut = fs.create(file, false);
+            return new IgnoreKeyLineRecordWriter<K, V>(new DataOutputStream
+                    (codec.createOutputStream(fileOut)),
+                    keyValueSeparator);
+        }
+    }
+}

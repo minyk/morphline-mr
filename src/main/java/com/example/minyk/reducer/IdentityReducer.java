31f401426b5bebf334ee60b0b274b63865194cb7
drakemin <drake.min@nexr.com>
2014-10-08 13:24:15 +0900

    Add IdentityReducer

---
+++ src/main/java/com/example/minyk/reducer/IdentityReducer.java
@@ -0,0 +1,18 @@
+package com.example.minyk.reducer;
+
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+
+/**
+ * Created by drake on 10/8/14.
+ */
+public class IdentityReducer extends Reducer<Text,Text,Text,Text>{
+    @Override
+    protected void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+        for(Text t : values) {
+            context.write(key,t);
+        }
+    }
+}
